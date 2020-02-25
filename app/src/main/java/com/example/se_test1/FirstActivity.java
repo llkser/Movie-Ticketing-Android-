3a6aa62a14a45dbbfab6924c441f697a2631de8f
Kang Liu <sc17kl@leeds.ac.uk>
2020-02-25 14:34:12 +0800

    login system1

---
+++ app/src/main/java/com/example/se_test1/FirstActivity.java
@@ -0,0 +1,13 @@
+package com.example.se_test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
